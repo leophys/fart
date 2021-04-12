7c044f76fd26939ee0f76fb2d3c370fbbac9149e
Federico Caprari <korsmakolnikov@gmail.com>
2021-04-12 20:17:50 +0200

    Refactor and bind filtering lists logic to the server

--- server/serve.go
+++ server/serve.go
@@ -5,12 +5,19 @@ import (
 	"net/http/httputil"
 
 	"github.com/elazarl/goproxy"
+	fmt "gopkg.in/ffmt.v1"
 )
 
 type ServeParams struct {
-	BindAddr      string
-	CtrlAddr      string
-	WebsocketAddr string
+	BindAddr        string
+	CtrlAddr        string
+	WebsocketAddr   string
+	BlacklistTarget []string
+	WhitelistTarget []string
+}
+
+type Session struct {
+	intercepted bool
 }
 
 func Server(params ServeParams) error {
@@ -18,23 +25,69 @@ func Server(params ServeParams) error {
 	proxy.Verbose = true
 	proxy.OnRequest().DoFunc(
 		func(r *http.Request, ctx *goproxy.ProxyCtx) (*http.Request, *http.Response) {
-			req, err := httputil.DumpRequest(r, true)
-			if err != nil {
-				ctx.Warnf("Error on request dumping: %s\n", err)
-				return r, nil
-			}
-			ctx.Logf("Request:\n---------\n%s\n---------\n", string(req))
-			return r, nil
+			return onRequest(&params, r, ctx)
 		})
 	proxy.OnResponse().DoFunc(
 		func(r *http.Response, ctx *goproxy.ProxyCtx) *http.Response {
-			resp, err := httputil.DumpResponse(r, true)
-			if err != nil {
-				ctx.Warnf("Error on response dumping: %s\n", err)
-				return r
-			}
-			ctx.Logf("Response:\n---------\n%s\n---------\n", string(resp))
-			return r
+			return onResponse(r, ctx)
 		})
+
 	return http.ListenAndServe(params.BindAddr, proxy)
+}
+
+func onRequest(params *ServeParams, r *http.Request, ctx *goproxy.ProxyCtx) (*http.Request, *http.Response) {
+	var s Session
+	var req []byte
+	var dumpErr error
+
+	fmt.Pjson(params)
+	fmt.Print(r.Host)
+
+	if 0 == len(params.WhitelistTarget) {
+		for _, p := range params.BlacklistTarget {
+			if p == r.Host {
+				return r, nil
+			}
+		}
+
+		s.intercepted = true
+
+	} else {
+		for _, p := range params.WhitelistTarget {
+			if p == r.Host {
+				s.intercepted = true
+			}
+		}
+	}
+
+	if s.intercepted {
+		req, dumpErr = httputil.DumpRequest(r, true)
+		if dumpErr != nil {
+			ctx.Warnf("Error on request dumping: %s\n", dumpErr)
+			return r, nil
+		}
+
+		ctx.UserData = s
+		ctx.Logf("Request:\n---------\n%s\n---------\n", string(req))
+	}
+
+	return r, nil
+}
+
+func onResponse(r *http.Response, ctx *goproxy.ProxyCtx) *http.Response {
+	resp, err := httputil.DumpResponse(r, true)
+	if err != nil {
+		ctx.Warnf("Error on response dumping: %s\n", err)
+
+		return r
+	}
+
+	switch ctx.UserData.(type) {
+	case Session:
+		ctx.Logf("Response:\n---------\n%s\n---------\n", string(resp))
+
+	default:
+	}
+
+	return r
 }
